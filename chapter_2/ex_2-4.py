82833fc109c5b862cb2af34084ddf36ae573cf56
Akshay Moharir <moharir.akshay@gmail.com>
2020-04-21 01:26:56 -0400

    Completed exercise 2.4, 2.5

---
+++ chapter_2/ex_2-4.py
@@ -0,0 +1,15 @@
+## Python Crash Course
+
+# Ex 2.4:  
+
+
+def main():
+    nameOfScientist = "Albert Einstein"
+    
+    print(nameOfScientist.lower())
+    print(nameOfScientist.upper())
+    print(nameOfScientist.title())
+
+if __name__ == '__main__':
+    main()
+
---
+++ chapter_2/ex_2-5.py
@@ -0,0 +1,19 @@
+## Python Crash Course
+
+# Ex 2.5:  Famous Quote: Find a quote from a famous person you admire. Print the
+# quote and the name of its author. Your output should look something like the
+# following, including the quotation marks:
+# Albert Einstein once said, A person who never made a mistake never tried anything new.
+
+
+
+def main():
+    nameOfScientist = "Albert Einstein"
+    connectingSentence = ' said, "'
+    quote_1 = 'Compound interest is the 8th wonder of the world. He who understands it, earns it; he who doesn\'t, pays it."'
+    
+    print(nameOfScientist + connectingSentence + quote_1)
+
+if __name__ == '__main__':
+    main()
+
