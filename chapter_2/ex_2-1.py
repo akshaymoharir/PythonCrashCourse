e00a5ebc932ae47ef39a7a414ca18735988326d3
Akshay Moharir <moharir.akshay@gmail.com>
2020-04-20 23:36:36 -0400

    Completed exercise 2.1 and 2.2 from chapter-2.

---
+++ chapter_2/ex_2-1.py
@@ -0,0 +1,12 @@
+
+## Python Crash Course
+
+# Ex 2.1: Simple Message: Store a message in a variable, and then print that message.
+
+def main():
+    message = "Alpha Bravo Charlie.."
+    print("Hello Akshay, this is a message for you: ", repr(message))   # print a string as a pritable representation of object
+
+if __name__ == '__main__':
+    main()
+
---
+++ chapter_2/ex_2-2.py
@@ -0,0 +1,16 @@
+
+## Python Crash Course
+
+# Ex 2.2: Simple Messages: Store a message in a variable, and print that message. 
+#           Then change the value of your variable to a new message, and print the new message.
+
+def main():
+    message = "This is a string in a variable named 'message'"
+    print(message)  
+
+    message = "Changing the string in the variable named 'message'"
+    print(message)
+
+if __name__ == '__main__':
+    main()
+
